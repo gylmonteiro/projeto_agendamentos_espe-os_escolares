7c10cb187358ab019f4aa6ea132fe2a7e1b82305
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-10-08 08:07:26 -0300

    feat: criado um menu de navegacao com links simples e buttons simples para distribuir a navegacao inicial

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def cadastrar_usuario(request):
-    return HttpResponse("aqui estou")
+    return render(request, 'formulario_login.html')
