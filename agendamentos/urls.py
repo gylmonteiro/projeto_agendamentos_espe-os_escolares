11a800512e587fab560e6fe23d84c26342ade8cc
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-10-13 16:39:37 -0300

    feat: criado a listagem e o formulario para enviar os dados do agendamento

--- agendamentos/urls.py
+++ agendamentos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.listar_agendamentos, name="lista_agendamentos"),
+    path("agendar_espaco", views.criar_agendamento, name="agenda_sala")
     
 ]
--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Agendamento
+from .models import Agendamento, Espaco, Horario
+from turmas.models import Turma
 # Create your views here.
 def listar_agendamentos(request):
     if request.method == "GET":
         agendamentos = Agendamento.objects.all()
         return render(request, "listagem_agendamentos.html", {"agendamentos": agendamentos})
+    
+
+def criar_agendamento(request):
+    espacos = Espaco.objects.all()
+    turmas = Turma.objects.all()
+    horarios = Horario.objects.all()
+    if request.method == "GET":
+        return render(request, 'formulario_agendamento.html', {"espacos": espacos, "turmas": turmas, "horarios": horarios})
