21503a065ba323899e4052a6be8c3f0d58e65911
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-10-14 10:59:42 -0300

    ajustar a lisgatem no template

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Agendamento, Espaco, Horario
 from turmas.models import Turma
+from datetime import datetime
 # Create your views here.
 def listar_agendamentos(request):
     if request.method == "GET":
@@ -24,11 +25,12 @@ def pesquisar_espaco(request):
     elif request.method == "POST":
         horarios = Horario.objects.all()
         data = request.POST.get('data')
+        data_formatada =  datetime.strptime(data, '%Y-%m-%d').date()
         espaco_id = request.POST.get('espaco')
         espaco_filter = Espaco.objects.filter(pk=espaco_id)[0]
         agendamentos_sala = Agendamento.objects.filter(espaco=espaco_filter)
-        
-        return render (request, "listagem_agendamentos_sala.html", {'espaco': espaco_filter, 'horarios': horarios, 'agendamentos_sala':agendamentos_sala, 'data': data} )
+
+        return render (request, "listagem_agendamentos_sala.html", {'espaco': espaco_filter, 'horarios': horarios, 'agendamentos_sala':agendamentos_sala, 'data': data_formatada} )
 
 # def lista_agendamentos_sala(request):
 #     if request.method == "POST":
