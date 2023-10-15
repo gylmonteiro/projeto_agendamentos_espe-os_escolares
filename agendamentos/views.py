d56af72211dc6da8ef1be61b2088be7d9504afc1
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-10-15 10:27:45 -0300

    feat: criado a listagem, fazendo uma renderização para os horários que estão ocupados e outra para os horários que não estão ocupados

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -11,11 +11,18 @@ def listar_agendamentos(request):
     
 
 def criar_agendamento(request):
+    espaco = request.GET.get('espaco')
+    print((espaco))
     espacos = Espaco.objects.all()
     turmas = Turma.objects.all()
     horarios = Horario.objects.all()
     if request.method == "GET":
         return render(request, 'formulario_agendamento.html', {"espacos": espacos, "turmas": turmas, "horarios": horarios})
+    
+    if request.method =="POST":
+        
+        
+        return HttpResponse("É aqui mesmo")
     
 
 def pesquisar_espaco(request):
@@ -23,15 +30,19 @@ def pesquisar_espaco(request):
     if request.method == "GET":
         return render (request, "pesquisa_espacos.html", {'espacos': espacos})
     elif request.method == "POST":
-        horarios = Horario.objects.all()
+        # receber todos os horaríos
+        horarios_disponiveis = Horario.objects.all()
+
+        # formatar data para o campo django
         data = request.POST.get('data')
         data_formatada =  datetime.strptime(data, '%Y-%m-%d').date()
+        
+        # filtrando espaço através dos dados originados do POST na pesquisa de filtragem
         espaco_id = request.POST.get('espaco')
         espaco_filter = Espaco.objects.filter(pk=espaco_id)[0]
         agendamentos_sala = Agendamento.objects.filter(espaco=espaco_filter, data_agendamento=data_formatada)
+        horarios_em_uso = agendamentos_sala.values_list('horario', flat=True)
+        horarios_disponiveis = horarios_disponiveis.exclude(id__in=horarios_em_uso)
 
-        return render (request, "listagem_agendamentos_sala.html", {'espaco': espaco_filter, 'horarios': horarios, 'agendamentos_sala':agendamentos_sala, 'data': data_formatada} )
+        return render (request, "listagem_agendamentos_sala.html", {'espaco': espaco_filter, 'horarios': horarios_disponiveis, 'agendamentos_sala':agendamentos_sala, 'data': data_formatada} )
 
-# def lista_agendamentos_sala(request):
-#     if request.method == "POST":
-#         return render(request, "listagem_agendamentos_sala.html")
