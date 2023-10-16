fb9a0c8e80887fb0a79c362a7e3a1b573e1a5063
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2023-10-16 13:47:24 -0300

    feat: alterado allowed hosts para realizar testes

--- agendamento_espacos_escolares/settings.py
+++ agendamento_espacos_escolares/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)a2fe4b@%w4+3!#@d7abh(^jw&s%nsjoxkrx975na%#v5n(&y*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
